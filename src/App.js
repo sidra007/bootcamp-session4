c015e44a4d2dcc5e0710e7a7da704f926ab414b4
sidra007 <engr.sidra@hotmail.com>
2020-09-26 12:57:43 +0500

    updated by adding counter button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import Message from './Message.js';
 
 function App(){
-  let[ismorning,setmorning]=useState(false)
+  let[ismorning,setmorning]=useState(false);
+  let[count,setcount]=useState(1);
   return(
   <div className = {`night ${ismorning ? `day`:``}`}>
   <Message time = {ismorning ? 'day':'night'}/>
+  <h1>the value of counter is {count}</h1>
+  <button onClick={()=>setcount(count+1)}>
+    start
+  </button>
   </div>
   );
 }
